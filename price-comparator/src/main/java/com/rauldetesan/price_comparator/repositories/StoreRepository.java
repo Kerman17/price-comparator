036a858c4d50c3fc500e6019ce3d072b4273577b
Detesan Raul <deteraul@yahoo.com>
2025-05-17 15:18:27 +0300

    Added a new method to StoreRepository for finding a store by id, and creating one if non-existent.

--- price-comparator/src/main/java/com/rauldetesan/price_comparator/repositories/StoreRepository.java
+++ price-comparator/src/main/java/com/rauldetesan/price_comparator/repositories/StoreRepository.java
@@ -4,7 +4,10 @@ import com.rauldetesan.price_comparator.domain.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface StoreRepository extends JpaRepository<Store, Long> {
 
+    Optional<Store> findByNameIgnoreCase(String storename);
 }
