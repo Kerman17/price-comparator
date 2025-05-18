74684402fbc796d821de4693da0e55e6a8f0d13e
Detesan Raul <deteraul@yahoo.com>
2025-05-18 17:39:09 +0300

    Deleted the entirety of Product entity, with its controller and repository - faulty and buggy function.

--- price-comparator/src/main/java/com/rauldetesan/price_comparator/repositories/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rauldetesan.price_comparator.repositories;
-
-import com.rauldetesan.price_comparator.domain.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, String> {
-}
